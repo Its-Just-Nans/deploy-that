5a3e11d14938c81481ab6f43c49f51806f4fbafb
n4n5 <its.just.n4n5@gmail.com>
2025-04-01 20:28:56 +0200

    test onw workflow

--- deployer.sh
+++ deployer.sh
@@ -7,4 +7,7 @@ cd "$DIRECTORY" || (echo "Directory $DIRECTORY not found" && exit 1)
 if [ "$LANGUAGE" = "javascript" ]; then
     npm install
     npm run build
+else
+    echo "Unsupported language: $LANGUAGE"
+    exit 1
 fi
