4d1e661c00ef8a6e7f7a5d7778160a9ec6f0d3f5
n4n5 <its.just.n4n5@gmail.com>
2025-04-01 20:39:21 +0200

    update

--- deployer.sh
+++ deployer.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# check if the LANGUAGE environment variable is set
+if [ -z "$LANGUAGE" ]; then
+    echo "The LANGUAGE environment variable is not set"
+    exit 1
+fi
+
+# check if the DIRECTORY environment variable is set
+if [ -z "$DIRECTORY" ]; then
+    echo "The DIRECTORY environment variable is not set"
+    exit 1
+fi
+
 echo "Deploying the $LANGUAGE application..."
 cd "$DIRECTORY" || (echo "Directory $DIRECTORY not found" && exit 1)
 
