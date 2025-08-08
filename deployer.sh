d9812828df3205c314202565dfaf0de92937319f
n4n5 <its.just.n4n5@gmail.com>
2025-08-08 09:23:06 -0600

    update

--- deployer.sh
+++ deployer.sh
@@ -17,10 +17,13 @@ cd "$DIRECTORY" || (echo "Directory $DIRECTORY not found" && exit 1)
 
 # if $LANGUAGE is "javascript"
 if [ "$LANGUAGE" = "javascript" ]; then
+    echo "npm install"
     npm install
+    echo "npm run build"
     npm run build
 elif [ "$LANGUAGE" = "repo" ]; then
     # move everything to dist/ including hidden files except .git using rsync
+    echo "Creating dist/"
     rsync -av --exclude='.git' . dist/
 else
     echo "Unsupported language: $LANGUAGE"
